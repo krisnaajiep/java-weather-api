2724e6ea13da4782de85c515362cf77a071a7361
krisnaajiep <krisnaajiep@gmail.com>
2025-04-29 22:06:30 +0700

    feat(app): add an HTTP server to App for basic request handling

--- src/main/java/com/krisnaajiep/App.java
+++ src/main/java/com/krisnaajiep/App.java
@@ -1,13 +1,28 @@
 package com.krisnaajiep;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import com.krisnaajiep.handler.WeatherHandler;
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.net.BindException;
+import java.net.InetSocketAddress;
+
+public class App {
+    public static void main(String[] args) {
+        try {
+            int port = args.length > 0 ? Integer.parseInt(args[0]) : 8080;
+            HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
+
+            server.createContext("/", new WeatherHandler());
+            server.setExecutor(null);
+            System.out.println("Server running on port " + port);
+            server.start();
+        } catch (NumberFormatException e) {
+            System.out.println("Port must be a number");
+        } catch (BindException e){
+            System.out.println(e.getMessage());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
